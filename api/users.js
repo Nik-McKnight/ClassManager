d05ca857f714275a6f118224710ff646eda3456a
Nik-McKnight <nik.mcknight735@gmail.com>
2022-12-05 13:02:34 -0700

    Users read and update routes.

--- api/users.js
+++ api/users.js
@@ -48,6 +48,104 @@ userRouter.post(
   }
 );
 
+userRouter.get("/read", userRequired, adminRequired, async (req, res, next) => {
+  const { email, id } = req.body;
+  let user;
+  try {
+    if (id) {
+      user = await prisma.User.findUnique({
+        where: {
+          id: +id,
+        },
+      });
+    } else if (email) {
+      user = await prisma.User.findUnique({
+        where: {
+          email: email,
+        },
+      });
+    }
+    if (user) {
+      res.send(user);
+    } else res.send(`No account found.`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.patch("/update", userRequired, async (req, res, next) => {
+  try {
+    const user = req.user;
+    const id = user.id;
+    const { first_name, last_name, preferred_name, address, phone, password } =
+      req.body;
+    const updatedUser = await prisma.User.update({
+      where: {
+        id,
+      },
+      data: {
+        first_name: first_name ? first_name : user.first_name,
+        last_name: last_name ? last_name : user.last_name,
+        preferred_name: preferred_name ? preferred_name : user.preferred_name,
+        address: address ? address : user.address,
+        phone: phone ? phone : user.phone,
+        password: password ? password : user.password,
+      },
+    });
+    res.send({ updatedUser });
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.patch(
+  "/update/:id",
+  userRequired,
+  adminRequired,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await prisma.User.findUnique({
+        where: {
+          id: +id,
+        },
+      });
+      if (user) {
+        const {
+          first_name,
+          last_name,
+          email,
+          preferred_name,
+          gpa,
+          address,
+          phone,
+          password,
+        } = req.body;
+        const updatedUser = await prisma.User.update({
+          where: {
+            id: +id,
+          },
+          data: {
+            first_name: first_name ? first_name : user.first_name,
+            last_name: last_name ? last_name : user.last_name,
+            email: email ? email : user.email,
+            preferred_name: preferred_name
+              ? preferred_name
+              : user.preferred_name,
+            gpa: gpa ? gpa : user.gpa,
+            address: address ? address : user.address,
+            phone: phone ? phone : user.phone,
+            password: password ? password : user.password,
+          },
+        });
+        res.send({ updatedUser });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 userRouter.post(
   "/delete",
   userRequired,
